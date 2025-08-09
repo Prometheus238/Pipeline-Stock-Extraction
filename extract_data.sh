32f3162037fecd23e580d5145b9d52db765d66e8
REEDHAM <69340526+Prometheus238@users.noreply.github.com>
2025-08-09 17:56:02 -0400

    Delete extract_data.sh

--- extract_data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Get the current month's first and last dates
-start_date=$(date -d "$(date +%Y-%m-01) -1 month" +%Y-%m-01)
-end_date=$(date -d "$start_date +1 month -1 day" +%Y-%m-%d)
-
-# Run the Python script with start date and end date as arguments
-python3 /path/to/extract_data.py "$start_date" "$end_date"
